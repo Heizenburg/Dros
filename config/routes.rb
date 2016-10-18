446c51fe31fa3043c535a19635f4bece4b6f5d15
Heizenburg <worlddc219@gmail.com>
2016-10-18 18:24:15 +0200

    Finished user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get '/' => 'static_pages#home', :as => "root"
-  
+  resources :users
 end
