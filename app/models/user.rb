74875f498cf4d38e12a0897a6d4daaec2468d6d9
Heizenburg <worlddc219@gmail.com>
2016-10-19 20:41:15 +0200

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+attr_accessor :remember_token
 before_save { email.downcase! }
 validates :name, presence: true, length: { maximum: 50 } # This is eq to validates(:name, presence: true) its just a method.
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,10 +11,38 @@ validates :email, presence: true, length: { maximum: 255 },
 has_secure_password
 validates :password, presence: true, length: { minimum: 6 }
 
+
 def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+# Returns a random token.
+ def User.new_token # tokens granted every time a user logs out and by taking care to cryptographically sign
+   SecureRandom.urlsafe_base64
+ end
+
+# Remembers a user in the database for use in persistent sessions.
+def remember
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
+end
+
+# Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    if remember_digest.nil?
+      false
+    else
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+    # Above line could also be return false if remember_digest.nil?
+    # BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
